c2598d09f76a7f1f1bef9faa8649d5198fbaa3fb
emily-chu <emily.megan.chu@gmail.com>
2020-08-09 09:39:34 -0700

    Created React frontend

---
+++ frontend/src/components/create-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+//import axios from 'axios';
+
+export default class CreateExercise extends Component {
+	render() {
+		return (
+			<div>
+				<p>CreateExercise component</p>
+			</div>
+		)
+	}
+}
---
+++ frontend/src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+//import axios from 'axios';
+
+export default class EditExercise extends Component {
+	render() {
+		return (
+			<div>
+				<p>EditExercise component</p>
+			</div>
+		)
+	}
+}
