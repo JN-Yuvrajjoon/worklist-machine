44ea9b562083e17fead0c4ebdb6e9896921a6dd2
emily-chu <emily.megan.chu@gmail.com>
2020-08-12 20:18:48 -0700

    Restructured components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+//import { Tabs, Tab } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Navbar from "./components-deprecated/navbar.component"
@@ -11,34 +12,47 @@ import CreateUser from "./components-deprecated/create-user.component";
 
 function App() {
 	return (
-		
-			<div class="container-fluid p-3">
+		<div class="container-fluid p-3"> {/* Main container */}
 			<div class="row">
-				<div class="col-md-3" id="worklist-machine-left-column">
-				<div class="shadow rounded-lg">
-				<ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
+				<div class="col-md-3" id="wm-input-column">
+					<div class="shadow rounded-lg">
+						<ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
+							
+							{/* Settings tab */}
+							<li class="nav-item" role="presentation">
+								<a class="nav-link active" id="settings-tab" data-toggle="tab" href="#settings-menu" role="tab" aria-controls="settings" aria-selected="true">
+								Settings
+							</a>
+							</li>
 
-					<li class="nav-item" role="presentation">
-						<a class="nav-link active" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="true">
-					Settings
-					</a>
-					</li>
+							{/* Courses tab */}
+							<li class="nav-item" role="presentation">
+								<a class="nav-link" id="courses-tab" data-toggle="tab" href="#courses-menu" role="tab" aria-controls="courses" aria-selected="false">Courses</a>
+							</li>
 
-					<li class="nav-item" role="presentation">
-						<a class="nav-link" id="courses-tab" data-toggle="tab" href="#courses" role="tab" aria-controls="courses" aria-selected="false">Courses</a>
-					</li>
+							{/* About tab */}
+							<li class="nav-item" role="presentation">
+								<a class="nav-link" id="about-tab" data-toggle="tab" href="#about-menu" role="tab" aria-controls="about" aria-selected="false">About</a>
+							</li>
 
-					<li class="nav-item" role="presentation">
-						<a class="nav-link" id="about-tab" data-toggle="tab" href="#about" role="tab" aria-controls="about" aria-selected="false">About</a>
-					</li>
+						</ul>
 
-				</ul>
-				<div class="tab-content p-3" id="myTabContent">
-					<div class="tab-pane fade show active" id="settings" role="tabpanel" aria-labelledby="settings-tab">settings</div>
-					<div class="tab-pane fade" id="courses" role="tabpanel" aria-labelledby="courses-tab">courses</div>
-					<div class="tab-pane fade" id="about" role="tabpanel" aria-labelledby="about-tab">about</div>
-				</div>
-			</div>
+						<div class="tab-content p-3" id="myTabContent">
+							
+							<div class="tab-pane fade show active" id="#settings-menu" role="tabpanel" aria-labelledby="settings-menu"> 
+								settings
+							</div>
+							
+							<div class="tab-pane fade" id="c#ourses-menu" role="tabpanel" aria-labelledby="courses-menu">
+								courses
+							</div>
+							
+							<div class="tab-pane fade" id="#about-menu" role="tabpanel" aria-labelledby="about-menu">
+								about
+							</div>
+
+						</div>
+					</div>
 				</div>
 				<div className="container-fluid col-md-9 p-0" id="worklist-machine-main">
 				<Router>
